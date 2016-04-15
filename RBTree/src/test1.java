a26a1fe0a3b06f45ac7a0611a623c6c4544236e5
galmw <Gal@Gals-MacBook-Pro.local>
2016-04-15 16:02:24 +0300

    all

--- RBTree/src/test1.java
+++ RBTree/src/test1.java
@@ -7,23 +7,29 @@ public class test1 {
 			t.insert(i, ""+i);
 			System.out.println(t.toString(t.getRoot()));
 		}
-	
-		t.delete(7);
-		System.out.println(t.toString(t.getRoot()));
-		/*int [] arr= t.keysToArray();
-		for (int i : arr)
-			System.out.println(i);*/
-		//System.out.println(t.toString(t.getRoot()));
 		System.out.println(t.search(1));
+		for (int i =0; i<10; i++) {
+			t.delete(i);
+			System.out.println(t.toString(t.getRoot()));
+		}		
 
-		//t.insert(5,"5");
-		
+
 		System.out.println(t.search(0));
 		System.out.println(t.search(1));
 		System.out.println(t.search(2));
 		System.out.println(t.search(6));
 		System.out.println(t.search(7));
+		int [] arr = t.keysToArray();
+		String [] arr2 = t.valuesToArray();
 		
+		for(int i: arr){
+			System.out.print(i +" ");
+		}
+		System.out.println(" ");
+
+		for(String i: arr2){
+			System.out.print(i +" ");
+		}
 
 	}
 }
